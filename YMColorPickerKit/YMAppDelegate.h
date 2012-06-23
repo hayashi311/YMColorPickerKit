d7d3b204b287c72796f9be04091668a3182f6095
Ryota hayashi <hayashi311@gmail.com>
2012-06-24 00:06:27 +0900

    Initial Commit

---
+++ YMColorPickerKit/YMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YMAppDelegate.h
+//  YMColorPickerKit
+//
+//  Created by 林 亮太 on 12/06/24.
+//  Copyright (c) 2012 Hayashi Ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YMColorPickerKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YMColorPickerKit
+//
+//  Created by 林 亮太 on 12/06/24.
+//  Copyright (c) 2012 Hayashi Ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YMAppDelegate class]));
+    }
+}
